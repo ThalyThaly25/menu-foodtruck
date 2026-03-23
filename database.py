c13778e4de82c39f87d7d25a78d94834129bde87
Thalia Reyes <reyesthalia722@gmail.com>
2026-03-22 20:23:50 -0600

    Change database connection to PostgreSQL
    Updated DATABASE_URL to use environment variable for PostgreSQL.

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./foodtruck.db"
+DATABASE_URL = os.getenv("postgresql://uncle2302:GO6uBqN2TyT7tFYtTLIEfqu7cdZjyq4G@dpg-d70a74hr0fns73cnkolg-a/unclemoro_db")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}  # necesario para SQLite
